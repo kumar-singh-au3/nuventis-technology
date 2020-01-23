58c6b322fda43364529fecdc71568c88d187aaea
Kumar Singh <omkarjadhav151@gmail.com>
2020-01-24 02:57:13 +0530

    Updates

---
+++ precache-manifest.0064c648ba67f04f1fc707abbf049792.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8953018817f857e9d1cefb62e4e9f84a",
+    "url": "/test-project/index.html"
+  },
+  {
+    "revision": "64cdaf82424b3b06e3b9",
+    "url": "/test-project/static/css/main.b1ddd80f.chunk.css"
+  },
+  {
+    "revision": "200fb3641ca110e289dd",
+    "url": "/test-project/static/js/2.c3aa988e.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/test-project/static/js/2.c3aa988e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "64cdaf82424b3b06e3b9",
+    "url": "/test-project/static/js/main.bd9e16c3.chunk.js"
+  },
+  {
+    "revision": "43cf7e4c5c7fe09239ba",
+    "url": "/test-project/static/js/runtime-main.4297a60f.js"
+  }
+]);
